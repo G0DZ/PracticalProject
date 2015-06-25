167add152920782086a9b5a7c14fc86e68d8d2cd
G0DZ <0apm.tv@gmail.com>
2015-06-25 23:50:54 +0300

    Изменения файла "Data.java"

--- PracticalProject/src/def/Data.java
+++ PracticalProject/src/def/Data.java
@@ -1,8 +1,18 @@
 package def;
 
+import javax.swing.JButton;
+
 public class Data 
 {
-	int[] array;	
+	int[] array;
+	/*
+	button_input = new JButton("Ввод исходных данных");
+	button_start_sort = new JButton("Запуск сортировки");
+	button_make_step = new JButton("Шаг");
+	button_check = new JButton("Проверка");
+	button_save = new JButton("Сохранение");
+	button_revival = new JButton("Восстановление");
+	*/
 	
 	Data() 
 	{
