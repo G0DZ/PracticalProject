574df63caa7bd25cb8b81d18eada27967bca557a
G0DZ <0apm.tv@gmail.com>
2015-06-26 03:09:40 +0300

    Алгоритм сортировки вставками перенесен в поток. Сделана простейшая визуализация в режиме демонстрации. Добавлена реакция на кнопку "Запуск сортировки".

--- PracticalProject/src/def/SortEngine.java
+++ PracticalProject/src/def/SortEngine.java
@@ -4,6 +4,7 @@ import java.awt.event.ActionListener;
 import java.awt.event.ActionEvent;
 
 import javax.swing.JButton;
+import javax.swing.JOptionPane;
 
 import java.io.File;
 import java.io.FileNotFoundException;
@@ -20,15 +21,52 @@ public class SortEngine implements ActionListener {
     	dataEngine = data;
     }
        	    
+    
+    
+    public boolean readArray(String fileName, int real_length) {
+    	
+    	try {
+	    	Scanner in = new Scanner(new File(fileName));
+	    	real_length = 0;
+	    	int size =  in.nextInt();
+	    	dataEngine.array = new int[size];
+			while(in.hasNextInt()){
+				dataEngine.array[real_length] = in.nextInt();	
+				real_length++;
+			}
+			in.close();	
+	    	return true;
+    	}
+    	catch (FileNotFoundException ex) {
+       		return false;
+    	}
+    }
+    	
     public void actionPerformed(ActionEvent e) {
     	
     	// Получение источника события
     	JButton clickedButton =  (JButton) e.getSource();
     	// Если это кнопка "Ввод исходных данных"
-    	if (clickedButton.getActionCommand() == dataEngine.b_input_txt) {
+    	String actioncommand = clickedButton.getActionCommand();
+    	if (actioncommand == dataEngine.b_input_txt) 
             Generation.createAndShowGENERATION(dataEngine);
+    	if(actioncommand == dataEngine.b_start_sort_txt)
+    	{ //если это кнопка "Запуск сортировки"
+    		int real_length = 0;
+    		boolean inforWasRead = readArray("input1.txt", real_length);	
+    		if (dataEngine.array == null)
+    		{ //если массив пустой, тогда
+    			JOptionPane.showMessageDialog(null, "alert", "alert", JOptionPane.ERROR_MESSAGE);
+    			return;
+    		}	
+			parent.vpanel.PanelArray = new int[dataEngine.array.length];
+			for (int i = 0; i < dataEngine.array.length; i++) {
+				parent.vpanel.PanelArray[i] = dataEngine.array[i];
+			}
+    		InsertionSort is1 = new InsertionSort(parent);
+    		is1.start();
     	}
-    } 
+    }
     
 }
 
