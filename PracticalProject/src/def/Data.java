e227225e47d15acbb5b72cd9b156869bdefa1b11
CherepkovaYuliya <166_yulya@mail.ru>
2015-06-26 06:58:51 +0300

    Добавлена связь между окнами (предок-потомок)

--- PracticalProject/src/def/Data.java
+++ PracticalProject/src/def/Data.java
@@ -9,7 +9,6 @@ public class Data
 	int[] array;
 	
 	String f_Sort_txt = "Сортировка";
-	String f_Generation_txt = "Ввод исходных данных";
 
 	String b_input_txt = "Ввод исходных данных";
 	String b_start_sort_txt = "Запуск сортировки";
@@ -18,12 +17,10 @@ public class Data
 	String b_save_txt = "Сохранение";
 	String b_revival_txt = "Восстановление";
 	String b_opt_for_txt = "Выбрать";
-	String b_OK_txt = "ОК";
+	String b_file_txt = "Файл";
+	String b_string_txt = "Строка ввода";
+	String b_auto_txt = "Автоматическая";
 	String b_cancel_txt = "Отмена";
-	
-	String rb_file_txt = "Файл";
-	String rb_string_txt = "Строка ввода";
-	String rb_auto_txt = "Автоматическая";
 	
 	Data() 
 	{
--- PracticalProject/src/def/Generation.java
+++ PracticalProject/src/def/Generation.java
@@ -7,113 +7,103 @@ import javax.swing.*;
 public class Generation extends JDialog implements ActionListener {
 	
 	JPanel windowGeneration;
-	JPanel cancelOKPanel;
-	JPanel radioPanel;
-	JRadioButton radio_file;
-    JRadioButton radio_string;
-    JRadioButton radio_auto;
-    JButton button_opt_for;
-    JButton button_OK;
+	JButton button_file;
+    JButton button_string;
+    JButton button_auto;
     JButton button_cancel;
+    Data dataGen;
 	private boolean result; // результат работы окна (true - нажатие кнопки OK, false - нажатие кнопки Отмена);
 	
 	public Generation(java.awt.Frame parent, Data data) {
 
 		super(parent, true); 
+		dataGen = data;		
 		
-		// Создаем панель (как бы главную панель)
+		// Создаем кнопочки
+		button_file = new JButton(dataGen.b_file_txt);
+		button_string = new JButton(dataGen.b_string_txt);
+		button_auto = new JButton(dataGen.b_auto_txt);
+		button_cancel = new JButton(dataGen.b_cancel_txt);
+		
+	    // Назначаем кнопочкам слушателей
+		button_file.addActionListener(this);
+		button_string.addActionListener(this);
+		button_auto.addActionListener(this);
+		button_cancel.addActionListener(this);
+
+				
+		// Добавляем кнопочки на панель
 		windowGeneration = new JPanel();
-    	// Говорим, что этой панелью будет управлять BorderLayout 
-    	BorderLayout blG = new BorderLayout(); 
-    	windowGeneration.setLayout(blG);
-
-        //Create the radio buttons.
-        radio_file = new JRadioButton(data.rb_file_txt);
-        radio_file.setSelected(true);
-        radio_string = new JRadioButton(data.rb_string_txt);
-        radio_auto = new JRadioButton(data.rb_auto_txt);
-        button_opt_for = new JButton(data.b_opt_for_txt);
-
-        //Group the radio buttons.
-        ButtonGroup group = new ButtonGroup();
-	    group.add(radio_file);
-	    group.add(radio_string);
-	    group.add(radio_auto);
-
-	    //Register a listener for the radio buttons.
-	    radio_file.addActionListener(this);
-	    radio_string.addActionListener(this);
-	    radio_auto.addActionListener(this);
-
-	    //Put the radio buttons in a column in a panel.
-	    radioPanel = new JPanel(new GridLayout(0, 1));
-	    radioPanel.add(radio_file);
-	    radioPanel.add(radio_string);
-	    radioPanel.add(radio_auto);
-	    
-	    button_OK = new JButton(data.b_OK_txt);
-	    button_cancel = new JButton(data.b_cancel_txt);
-	    cancelOKPanel = new JPanel();
-	    FlowLayout fL = new FlowLayout(); 
-    	windowGeneration.setLayout(fL);
-    	cancelOKPanel.add(button_OK);
- 	    cancelOKPanel.add(button_cancel);
-
-		windowGeneration.add("North",radioPanel);
-	    button_opt_for.setPreferredSize(new Dimension(15, 10));
-	    windowGeneration.add("Center",button_opt_for);
-	    windowGeneration.add("South",cancelOKPanel);
-	    
-	    
-	          
-	    // Создаем фрейм, задаем ему название и размещаем там нашу главную панель          
-	    JFrame frame = new JFrame(data.f_Generation_txt);
-		frame.setContentPane(windowGeneration);
-		   
-        //Display the window.
-        frame.pack();
+		GroupLayout grl = new GroupLayout(windowGeneration);
+		windowGeneration.setLayout(grl);
+		// Automatic gap insertion:
+		grl.setAutoCreateGaps(true);
+    	grl.setAutoCreateContainerGaps(true); 	
+    	
+    	grl.setHorizontalGroup(
+    			grl.createSequentialGroup()
+    			      .addGroup(grl.createParallelGroup(GroupLayout.Alignment.CENTER)
+    			    		.addComponent(button_file)
+    	    			    .addComponent(button_string)
+    	    			    .addComponent(button_auto)
+    	    			    .addComponent(button_cancel))
+    			);
+    	grl.setVerticalGroup(
+    			grl.createSequentialGroup()
+    				  .addComponent(button_file)
+    	    		  .addComponent(button_string)
+    	    		  .addComponent(button_auto)
+    	    		  .addComponent(button_cancel)
+    			);
+    	
+		this.add(windowGeneration);
+		
+		// Сделать размер окна подходящим
+        this.pack();
 	    // Запрет на изменение размера экрана
-	    frame.setResizable(false);
-	        
+	    this.setResizable(false);		
 	    //Точка размещения экрана
 	    Point center = GraphicsEnvironment.getLocalGraphicsEnvironment().getCenterPoint();
-	    int w = frame.getWidth();
-	    int h = frame.getHeight();
+	    int w = this.getWidth();
+	    int h = this.getHeight();
 	    center.x = center.x - w/2; 
 	    center.y = center.y - h/2;
-	    frame.setLocation(center);
-	    
-	    frame.setVisible(true);
+	    this.setLocation(center);
 	}
 	
+	public boolean execute() { // метод для "общения" с родительским окном
+		this.setVisible(true); // прорисовать дочернее окно; в данном месте выполнение программы приостановится, ожидая окончания работы пользователя (в нашем случае - функции dispose())
+        return this.result; // вернуть в качестве результата условное значение нажатой кнопки
+	}
 	
 	 public void actionPerformed(ActionEvent e) {
 		 
-		 
-		 
-		 
-		 
-		 /*
-			 * 
-			
-			    private void jButtonOKMouseClicked(java.awt.event.MouseEvent evt) { // обработка нажатия клавиши ОК
-					this.result = true; // пользователь нажал ОК
-					this.dispose(); // уничтожить окно
-			    }
-			 
-				private void jButtonCancelMouseClicked(java.awt.event.MouseEvent evt) {
-					this.result = false; // пользователь нажал Отмена
-					this.dispose(); // уничтожить окно
-				}
-			       
-			        public boolean execute() { // метод для "общения" с родительским окном
-			                this.setVisible(true); // прорисовать дочернее окно; в данном месте выполнение программы приостановится, ожидая окончания работы пользователя (в нашем случае - функции dispose())
-					return this.result; // вернуть в качестве результата условное значение нажатой кнопки
-			         }
-			*/
-		 
-		 
-	    
-	    } 
+		// Получение источника события
+	    JButton clickedButton =  (JButton) e.getSource();
+	    String actioncommand = clickedButton.getActionCommand();
+	    if (actioncommand == dataGen.b_file_txt) {
+	    	
+	    	this.result = true; 
+			this.dispose(); // уничтожить окно		    		
+	    }
+	    if (actioncommand == dataGen.b_string_txt) {
+	    	
+	    	
+	    	
+	    	this.result = true; 
+			this.dispose(); // уничтожить окно	    	
+	    }
+	    if (actioncommand == dataGen.b_auto_txt) { 
+	    	
+	    	
+	    	
+	    	this.result = true; 
+			this.dispose(); // уничтожить окно	    	
+	    }
+	    if (actioncommand == dataGen.b_cancel_txt) {
+	    	this.result = false; // пользователь нажал Отмена
+			this.dispose(); // уничтожить окно
+	    }
+	} 
 
 }
